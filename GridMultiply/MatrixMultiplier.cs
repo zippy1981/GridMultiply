9c431b4e9ba24cbe6f6748cc7247215a63820f0c
Justin Dearing <zippy1981@gmail.com>
2013-11-18 20:50:35 -0500

    Added about call. Separated MatrixMultiplier to a new call.

---
+++ GridMultiply/MatrixMultiplier.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using fastJSON;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Generic;
+using Nancy;
+
+namespace GridMultiply
+{
+    public class MatrixMultiplier : NancyModule
+    {
+        public MatrixMultiplier()
+        {
+            Get["/(About|Help)"] = parameters => JSON.Instance.ToJSON(new Dictionary<string, object>
+            {
+                {"Name", "GridMultiplier"}, 
+                { "Author", "Justin Dearing @zippy1981"},
+                { "Makes Use Of", new Dictionary<string, string>
+                    {
+                        { "fastJSON", "https://fastjson.codeplex.com/" },
+                        { "Math.NET Numerics", "http://www.mathdotnet.com/" }
+                    }
+                }
+            });
+            Get["/Multiplier/{Id}"] = parameters => JSON.Instance.ToJSON(DoMatrixMultiply(parameters.Id).ToArray());
+        }
+
+        private const double CORRELATION = 0.5;
+        private const int MATRIX_SIZE = 50;
+        private const int SERIES_SIZE = 10000;
+
+        private static Matrix<double> DoMatrixMultiply(int iter)
+        {
+            Console.WriteLine("Iterator: {0}", iter);
+
+            var choleskyInput = new double[50, 50];
+            var randomSeries = new double[SERIES_SIZE, MATRIX_SIZE];
+            for (var i = 0; i < MATRIX_SIZE; i++)
+                for (var j = 0; j < MATRIX_SIZE; j++)
+                    choleskyInput[i, j] = i == j ? 1d : CORRELATION;
+
+            var matrix = DenseMatrix.OfArray(choleskyInput);
+            var factorization = matrix.Transpose().Cholesky();
+
+            for (var i = 0; i < MATRIX_SIZE; i++)
+            {
+                var randomNumbers = new Normal();
+                for (var j = 0; j < SERIES_SIZE; j++)
+                    randomSeries[j, i] = randomNumbers.Sample();
+            }
+
+            var randomMatrix = DenseMatrix.OfArray(randomSeries);
+            var choleskiedOutput = randomMatrix * factorization.Factor;
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(choleskiedOutput.ToString());
+            return choleskiedOutput;
+        }
+    }
+}
--- GridMultiply/Program.cs
+++ GridMultiply/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using fastJSON;
 using MathNet.Numerics;
 using MathNet.Numerics.Algorithms.LinearAlgebra.Mkl;
-using MathNet.Numerics.Distributions;
-using MathNet.Numerics.LinearAlgebra.Double;
-using MathNet.Numerics.LinearAlgebra.Generic;
-using Nancy;
 
 namespace GridMultiply
 {
@@ -23,46 +18,6 @@ namespace GridMultiply
             Console.Write("Press any key to continue . . .");
             Console.ReadKey(true);
             nancyHost.Stop();
-        }
-    }
-
-    public class MatrixMultiplier : NancyModule
-    {
-        public MatrixMultiplier()
-        {
-            Get["/Multiplier/{Id}"] = parameters => JSON.Instance.ToJSON(DoMatrixMultiply(parameters.Id).ToArray());
-        }
-
-        private const double CORRELATION = 0.5;
-        private const int MATRIX_SIZE = 50;
-        private const int SERIES_SIZE = 10000;
-
-        private static Matrix<double> DoMatrixMultiply(int iter)
-        {
-            Console.WriteLine("Iterator: {0}", iter);
-
-            var choleskyInput = new double[50, 50];
-            var randomSeries = new double[SERIES_SIZE, MATRIX_SIZE];
-            for (var i = 0; i < MATRIX_SIZE; i++)
-                for (var j = 0; j < MATRIX_SIZE; j++)
-                    choleskyInput[i, j] = i == j ? 1d : CORRELATION;
-
-            var matrix = DenseMatrix.OfArray(choleskyInput);
-            var factorization = matrix.Transpose().Cholesky();
-
-            for (var i = 0; i < MATRIX_SIZE; i++)
-            {
-                var randomNumbers = new Normal();
-                for (var j = 0; j < SERIES_SIZE; j++)
-                    randomSeries[j, i] = randomNumbers.Sample();
-            }
-
-            var randomMatrix = DenseMatrix.OfArray(randomSeries);
-            var choleskiedOutput = randomMatrix * factorization.Factor;
-
-            Console.WriteLine("Result:");
-            Console.WriteLine(choleskiedOutput.ToString());
-            return choleskiedOutput;
         }
     }
 }
